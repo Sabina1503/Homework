d24f7055c22cc8a35c7744aa3579fb3ec2513d60
Sabina <sabinaheybatovaa@gmail.com>
2021-05-26 13:24:32 +0400

    sabina-26-05

---
+++ Salimo/script.js
@@ -0,0 +1,79 @@
+
+  $(function(){
+    $(".hover-btn").mouseenter(function(){
+      $(".galery").css({"transform":"translateX(0px)","opacity":"1","transition":".5s"});
+    });
+    $(".hover-btn").mouseleave(function(){
+      $(".galery").css({"transform":"translateX(-20px)","opacity":"0"});
+    });
+    $(".hover-btn").mouseenter(function(){
+      $(".envlp1").css({"transform":"translateX(10px)","opacity":"1","transition":".5s"});
+    });
+    $(".hover-btn").mouseleave(function(){
+      $(".envlp1").css({"transform":"translateX(-20px)","opacity":"0"});
+    });
+    $(".hover-btn").mouseenter(function(){
+      $(".fa-file-alt").css({"transform":"translateX(10px)","opacity":"1","transition":".5s"});
+    });
+    $(".hover-btn").mouseleave(function(){
+      $(".fa-file-alt").css({"transform":"translateX(-20px)","opacity":"0"});
+    });
+    $(".hover-btn").mouseenter(function(){
+      $(".fa-telegram-plane").css({"transform":"translateX(10px)","opacity":"1","transition":".5s"});
+    });
+    $(".hover-btn").mouseleave(function(){
+      $(".fa-telegram-plane").css({"transform":"translateX(-20px)","opacity":"0"});
+    });
+    $(".home-btn").mouseenter(function(){
+      $(".fa-arrow-down").css({"transform":"translateX(0px)","opacity":"1","transition":".5s"});
+    });
+    $(".home-btn").mouseleave(function(){
+      $(".fa-arrow-down").css({"transform":"translateX(-20px)","opacity":"0"});
+    });
+    $(".hover-li1").mouseenter(function(){
+      $(".fa-twitter").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li1").mouseleave(function(){
+      $(".fa-twitter").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li2").mouseenter(function(){
+      $(".fa-facebook-f").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li2").mouseleave(function(){
+      $(".fa-facebook-f").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li3").mouseenter(function(){
+      $(".fa-google-plus-g").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li3").mouseleave(function(){
+      $(".fa-google-plus-g").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li4").mouseenter(function(){
+      $(".fa-instagram").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li4").mouseleave(function(){
+      $(".fa-instagram").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li5").mouseenter(function(){
+      $(".fa-linkedin-in").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li5").mouseleave(function(){
+      $(".fa-linkedin-in").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li6").mouseenter(function(){
+      $(".fa-youtube").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li6").mouseleave(function(){
+      $(".fa-youtube").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+    $(".hover-li3").mouseenter(function(){
+      $(".fa-skype").css({"transform":"rotateY(-360deg)","color":"white","transition":".7s"});
+    });
+    $(".hover-li3").mouseleave(function(){
+      $(".fa-skype").css({"transform":" rotateY(0deg)","color":"orange","transition":".7s"});
+    });
+  });
+
+
+ 
+  $(".numb").counterUp({delay:10,time:1000});
